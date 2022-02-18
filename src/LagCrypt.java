52b6a26632662031c61dffe0e423461c9ab68d14
lag-spike <satarhassni0@gmail.com>
2022-02-17 22:07:47 -0500

    added gui

--- src/LagCrypt.java
+++ src/LagCrypt.java
@@ -6,8 +6,9 @@ public class LagCrypt {
         File file = new File(System.getProperty("user.dir") + "/files/myFile.txt");
         EncryptionTool et = new EncryptionTool();
 
+        GUI gui = new GUI();
         //et.encrypt(file,209);
-        et.decrypt(file, 209);
-        System.out.println(et.getMD5(file));
+        //et.decrypt(file, 209);
+        //System.out.println(et.getMD5(file));
     }
 }
